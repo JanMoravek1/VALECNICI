9bd6a12ce558f86dc61a395efbbf8658ac34d03c
janmoravek <jan.moravek1@gmail.com>
2022-03-18 17:21:04 +0100

    promenna postavy a ochrana

--- Valecnici/Program.cs
+++ Valecnici/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Valecnici;
 
 namespace Battle
@@ -11,54 +12,65 @@ namespace Battle
             Kouzelnik k = new Kouzelnik("Merlin", 6);
             Lucisnik l = new Lucisnik("Legolas", 4);
 
-            Console.WriteLine("Vítej ve Hře, boj 1 je Bojovnik vs. Kouzelnik, boj 2 je Kouzelnik vs. Lucisnik, boj 3 je Lucisnik vs Bojovnik");
+            List<Postava> postavy = new List<Postava>();
+            postavy.Add(b);
+            postavy.Add(k);
+            postavy.Add(l);
+
+            //Console.WriteLine("Vítej ve Hře, boj 1 je Bojovnik vs. Kouzelnik, boj 2 je Kouzelnik vs. Lucisnik, boj 3 je Lucisnik vs Bojovnik");
 
             BattleUtils.vypisZivotu(b, k, l);
 
-            Console.WriteLine("Zadej číslo boje, kdo má s kým bojovat: ");
-            int cisloBoje = int.Parse(Console.ReadLine());
+            Postava p1 = null;
+            Postava p2 = null;
 
-            switch (cisloBoje)
+            int cislo1;
+
+            do
+
             {
+                Console.WriteLine("Zadej číslo druhého bojovníka bojovník(0), kouzelník(1), lučišník(2)");
+                cislo1 = int.Parse(Console.ReadLine());
 
-                case 1:
+                if (cislo1 > postavy.Count - 1)
+                    Console.WriteLine("Může zadat pouze čísla od 0 - 2, zkuste znovu");
 
-                    do
-                    {
-                        b.ZautocNa(k);
-                        k.ZautocNa(b);
-                        BattleUtils.vypisZivotu(b, k, l);
-                    } while (b.PocetZivotu == 0 || b.PocetZivotu < 0 || k.PocetZivotu == 0 || k.PocetZivotu < 0);
-                    break;
+                else Console.WriteLine("Zvolili jste " + cislo1);
 
-                case 2:
+            } while (cislo1 > postavy.Count - 1);
 
-                    do
-                    {
-                        l.ZautocNa(k);
-                        k.ZautocNa(l);
-                        BattleUtils.vypisZivotu(b, k, l);
-                    } while (l.PocetZivotu == 0 || l.PocetZivotu < 0 || k.PocetZivotu == 0 || k.PocetZivotu < 0);
-                    break;
+            p1 = postavy[cislo1];
 
-                case 3:
+            int cislo2;
 
-                    do
-                    {
-                        l.ZautocNa(b);
-                        b.ZautocNa(l);
-                        BattleUtils.vypisZivotu(b, k, l);
-                    } while (b.PocetZivotu == 0 || b.PocetZivotu < 0 || l.PocetZivotu == 0 || l.PocetZivotu < 0);
-                    break;
+            do
 
-            }
+            {
+                Console.WriteLine("Zadej číslo druhého bojovníka bojovník(0), kouzelník(1), lučišník(2)");
+            cislo2 = int.Parse(Console.ReadLine());
+
+                if (cislo2 > postavy.Count - 1)
+                    Console.WriteLine("Může zadat pouze čísla od 0 - 2, zkuste znovu");
+
+                else if (cislo2 == cislo1) 
+                    Console.WriteLine("Nemůžete zvolit stejného bojovníka, zkuste znovu");
+
+                else Console.WriteLine("Zvolili jste " + cislo2);
+
+            } while (cislo2 > postavy.Count - 1 || cislo2 == cislo1);
+
+   
+            p2 = postavy[cislo2];
+
+            do
+            {
+                p1.ZautocNa(p2);
+                p2.ZautocNa(p1);
+                BattleUtils.vypisZivotu(b, k, l);
+            } while (p1.PocetZivotu > 0 && p2.PocetZivotu > 0);
 
             
-
-            BattleUtils.vypisZivotu(b, k, l);
             Console.WriteLine("Konec Hry ");
-
-
 
         }
 
