db59ea2b12444395c31af63305853e496feb386e
janmoravek <jan.moravek1@gmail.com>
2022-04-04 17:18:55 +0200

    uprava hry o Black Jack

---
+++ Valecnici/Banker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valecnici
+{
+    public class Banker
+    {
+        public string Jmeno { get; set; }
+        public int PocetBodu { get; set; }
+
+        virtual public void Hra()
+
+        {
+
+            Random nh = new Random();
+            int cislo = nh.Next(0, 12);
+
+            Console.WriteLine(Jmeno + " vrha kostkou a hodil" + cislo);
+
+            PocetBodu = PocetBodu + cislo;
+        }
+
+
+
+        public Banker (string jmeno, int pocetBodu)
+        {
+            this.Jmeno = jmeno;
+            this.PocetBodu = pocetBodu; // diky this budeme prirazovat do property, this znamena reference na kontretni instanci Postavy
+
+        }
+    }
+}
---
+++ Valecnici/Hra.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valecnici
+{
+    public class Hra
+    {
+
+        Banker bankerB = new Banker("Banker", 0);
+        Hrac hracH = new Hrac("Hrac", 0);
+
+        public void Hrej()
+
+        {
+
+            do
+
+            {
+                Console.WriteLine(" Hra Black Jack zacina, prohrava ten kdo se jako prvni dostane pres cislo 21");
+
+                bankerB.Hra ();
+                hracH.Hra();      
+
+            } while (bankerB.PocetBodu < 22 && hracH.PocetBodu < 22);
+
+            Console.WriteLine(bankerB.Jmeno + " ma Bodu: " + bankerB.PocetBodu);
+            Console.WriteLine(hracH.Jmeno + " ma Bodu: " + hracH.PocetBodu);
+
+            if (bankerB.PocetBodu > 21)
+                                Console.WriteLine("Vyhral: " + bankerB.Jmeno);
+
+            else Console.WriteLine("Vyhral: " + hracH.Jmeno);
+
+
+        }
+
+
+
+        
+    }
+}
